e4e046fd3372f561c673876ae67ca1a056bbc9d8
Adam Bissonnette <adam@mediamanifesto.com>
2020-04-18 22:41:52 -0600

    initial commit

---
+++ mudbot.py
@@ -0,0 +1,15 @@
+import louie
+import threading
+import time
+import atexit
+
+class MudBot():
+	def __init__(self):
+		#instantiate everything
+
+	
+
+
+if __name__ == '__main__':
+	M = MudBot()
+	m.main()
